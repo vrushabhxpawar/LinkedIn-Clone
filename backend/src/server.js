99174b85e4309885bf22fb9f1c531bdba85d43d8
VRUSHABH PAWAR <vrushabhpawar05@gmail.com>
2025-09-29 12:31:09 +0530

    Initialize backend with package.json and essential dependencies

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,14 +5,10 @@ import { connectDB } from "../db/connectDB.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import path from "path";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
 
 const app = express();
 const PORT = process.env.PORT;
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
+const __dirname = path.resolve();
 
 if (process.env.NODE_ENV !== "production") {
   app.use(
@@ -26,9 +22,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ limit: "5mb" }));
 app.use(cookieParser());
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+  await connectDB();
   console.log(`SERVER : http://localhost:${PORT}`);
-  connectDB();
 });
 
 import authRouter from "../routes/auth.routes.js";
@@ -38,19 +34,15 @@ import notificationRouter from "../routes/notification.routes.js";
 import connectionRouter from "../routes/connection.routes.js";
 
 if (process.env.NODE_ENV === "production") {
- app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.get(/.*/, (req, res) => {
-  res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
-});
-
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, "/frontend/dist/index.html"));
+  });
 }
-
 
 app.use("/api/auth", authRouter);
 app.use("/api/users", userRouter);
 app.use("/api/post", postRouter);
 app.use("/api/notification", notificationRouter);
 app.use("/api/connection", connectionRouter);
-
-
