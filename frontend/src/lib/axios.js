de7ae084104c559ec52b2870306cb501cd46c5db
Vrushabh Pawar <vrushabhpawar05@gmail.com>
2025-09-29 11:31:09 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-  baseURL : "http://localhost:3000/api",
+  baseURL : "https://linkedin-clone-backend-ijhj.onrender.com/",
   withCredentials : true
 })
